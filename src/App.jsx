b83ce5c93ab495cc014dcd83e4f668564da02152
Yonatan Ariel <yonatanriel@gmail.com>
2025-06-30 04:16:27 +0300

    styling + add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import UserList from "./components/userList/UserList";
 import Chat from "./components/chat/Chat";
+import styles from "./App.module.css";
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState("userList");
 
   return (
-    <div className="app">
+    <div className={styles.appConatiner}>
       {currentScreen === "userList" ? <UserList /> : <Chat />}
     </div>
   );
---
+++ src/components/UserIistItem.jsx
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import styles from "./userListItem/UserListItem.module.css";
+import { getFormatTime } from "../utils/getFormatTime";
+
+function UserListItem({ user }) {
+  const formattedTime = useMemo(
+    () => getFormatTime(user.lastMessageTime),
+    [user.lastMessageTime]
+  );
+
+  return (
+    <div className={styles.userListItemContainer}>
+      <img
+        src={user.profilePic}
+        alt={user.name}
+        className={styles.profilePic}
+      />
+      <div className={styles.userInfo}>
+        <h3 className={styles.userName}>{user.name}</h3>
+        <span className={styles.lastMessageTime}>{formattedTime}</span>
+      </div>
+    </div>
+  );
+}
+export default UserListItem;
